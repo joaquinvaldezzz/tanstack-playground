c8eaf670bacac7755e8ad501f409a141518753d3
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-12-21 09:49:52 +0800

    feat: create withTimeout utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,14 @@ export async function sleep(ms: number) {
     setTimeout(resolve, ms);
   });
 }
+
+export async function withTimeout<T>(promise: Promise<T>, ms: number) {
+  const timeout = new Promise<never>((_, reject) => {
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      reject(new Error(`Operation timed out after ${ms} ms`));
+    }, ms);
+  });
+
+  return Promise.race([promise, timeout]);
+}
