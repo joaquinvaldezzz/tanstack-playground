25759f6a74a62eb3caa61ead55bc4c25865438c0
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-12-21 09:45:31 +0800

    feat: create sleep utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,9 @@ export function cn(...inputs: ClassValue[]) {
 export function uid() {
   return Math.random().toString(36).slice(2, 10);
 }
+
+export async function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
